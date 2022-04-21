fc1ca799652b4f83d83f76beb5b14b32051f5b28
HakkyHakk <kanchnit@gmail.com>
2022-04-21 15:38:24 +0700

    update web

---
+++ js/vanta.waves.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+    el: ".bg-page",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x212121,
+    shininess: 0.00,
+    waveHeight: 40.00,
+    waveSpeed: 0.25,
+    zoom: 1.75
+  })
